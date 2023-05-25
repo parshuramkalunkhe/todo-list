bff2b9497540b2394de51d13f294ef1e50a5a16d
parshuram kalunkhe <parshuramkalunkhe@gmail.com>
2023-05-25 22:19:54 +0530

    Add stuff

--- app.js
+++ app.js
@@ -1,23 +1,63 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const date = require(__dirname + "/date.js");
 
 const app = express();
 const items = [];
-app.set("view engine", "ejs"); 
-app.use(bodyParser.urlencoded({extended: true}))
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser: true });
+
+const itemSchema = {
+    name: String
+};
+
+const Item = mongoose.model("Item", itemSchema);
+
+const item1 = new Item({
+    name: "Welcome to your todo-list!"
+});
+
+const item2 = new Item({
+    name: "Hit the + to add a new item."
+});
+
+const item3 = new Item({
+    name: "Your ready to go."
+});
+
+const defaultItems = [item1, item2, item3];
 
 app.get("/", function (req, res) {
 
     const day = date.getDate();
-    res.render("list",{ kindOfDay: day, newListItem: items });
+
+    Item.find({}).then(function (foundItems) {
+        if (foundItems.length === 0) {
+            Item.insertMany(defaultItems).then(function () {
+                console.log("Successfully saved defult items to DB");
+            }).catch(function (err) {
+                console.log(err);
+            });
+            res.redirect("/");
+        } else {
+            res.render("list", { kindOfDay: day, newListItem: foundItems });
+        }
+    }).catch(function(err){
+        console.log(err);
+    });
 });
 
 
-app.post("/", function(req, res) {
-    var item = req.body.newItem;
-    items.push(item)
+app.post("/", function (req, res) {
+    const itemName = req.body.newItem;
+    const item = Item({
+        name: itemName
+    });
+    item.save();
     res.redirect("/");
 });
 
